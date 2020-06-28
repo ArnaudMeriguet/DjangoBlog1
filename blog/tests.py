7a4289452964d6ac003464b7bfbd0aaa9f516f66
Wardov <marnaudm@gmail.com>
2020-06-28 20:51:03 +0100

    End of Ch3 of obeythetestinggoat: Added test for cv page. added a blank cv page (bad habbit, not supposed to do that in real TDD?)

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from blog.views import post_list, cv_view
 
 # Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found=resolve('/')
+        self.assertEqual(found.func,post_list)
+
+class CvPageTest(TestCase):
+
+    def test_cv_url_resolves_to_cv_page_view(self):
+        found=resolve('/post/cv/')
+        self.assertEqual(found.func,cv_view)
+
+    def test_cv_page_returns_correct_html(self):
+        request =HttpRequest()
+        response= cv_view(request)
+        html=response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>Arnaud Meriguet</title>',html)
+        self.assertTrue(html.endswith('</html>'))
