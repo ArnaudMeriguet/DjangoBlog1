66a5cf75c2b7db67da78d3d6240098c6f37a2d2e
Wardov <marnaudm@gmail.com>
2020-06-29 16:28:39 +0100

    refactor cv view page to use a template

--- blog/tests.py
+++ blog/tests.py
@@ -7,19 +7,11 @@ from blog.views import post_list, cv_view
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func,post_list)
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'blog/post_list.html')
 
 class CvPageTest(TestCase):
 
-    def test_cv_url_resolves_to_cv_page_view(self):
-        found=resolve('/post/cv/')
-        self.assertEqual(found.func,cv_view)
-
     def test_cv_page_returns_correct_html(self):
-        request =HttpRequest()
-        response= cv_view(request)
-        html=response.content.decode('utf8')
-        self.assertTrue(html.startswith('<!DOCTYPE html>'))
-        self.assertIn('<title>Arnaud Meriguet</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+        response= self.client.get('/post/cv/')
+        self.assertTemplateUsed(response, 'blog/cv_view.html')
--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,21 +14,36 @@ class NewVisitorTest(unittest.TestCase):
     def test_startAndRetrieveList(self):
 
         #Want to access app
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get('http://127.0.0.1:8000/post/cv')
 
-        #has "Arnaud Meriguet" in the header
+        #has "Arnaud Meriguet" in the titel and header
         self.assertIn ('Arnaud Meriguet', self.browser.title)
-        self.fail("FINISH TEST")
+        header_text=self.browser.find_elements_by_tag_name('h1').link
+        self.assertEqual('Arnaud Meriguet\'s Blog',header_text)
+        
+        #has 'CV' in the h2 tag
+        cv_h2_text=self.browser.find_element_by_id('CVh2').text
+        self.assertEqual('CV',cv_h2_text)
 
+        #The user is invited to add a skill to the list
+        inputbox=self.browser.find_element_by_id('idNewSkill')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a skill')
 
-        #in the CV page, the user enters an skill
-
+        #in the CV page, the user enters a skill
         #types "Python" into a textbox
+        inputbox.send_keys('Python')
 
         #hits Enter, page updates, page has now a skill named "Python"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table=self.browser.find_element_by_id('id_list_skills')
+        rows=table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text=='Python' for row in rows))
 
         #the box is still here waiting for more imputs
         #User inputs "Funtional testing"
+        self.fail('FINISH THE TEST!')
 
         #page updates, both "python" and "Functional testing" can be seen on the CV
 
